b8aa23b4202415e23e8fd6cf43f9e9fdd9e4b2ad
Esadruhn <heolio@hotmail.fr>
2025-03-12 15:15:00 +0100

    add posts stats and outliers

--- 3_sql_eda/README.md
+++ 3_sql_eda/README.md
@@ -12,3 +12,16 @@ Use Python 3.12
 pip install -r requirements.txt
 python main.py
 ```
+
+Null values: only in the `follows` and `plays columns`:
+
+- follows: 5 /  446 posts
+- plays: 314 / 446 posts
+- data comments: 446 / 446 posts
+
+We will not consider the `plays` or `data comments` category as there are too many unknown values.
+The number of null follows is low, might ask why there are some, I would assume that a null value means 0 follows.
+
+Most posts are of the carousel type, but the image posts get more impressions on average and the reel posts more likes, shares and follows.
+A quarter of the posts get a lot of visibility (impressions): 100 / 446 have between 30 and 500K impressions, there are 57 outliers in the data.
+
--- 3_sql_eda/main.py
+++ 3_sql_eda/main.py
@@ -2,8 +2,6 @@ import sqlite3
 import pathlib
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.express as px
-import plotly.io as pio
 from plotly.subplots import make_subplots
 
 
@@ -49,72 +47,251 @@ def init_db(connection):
     cursor.close()
 
 
-def analyse(cursor):
+def analyse_posts(cursor):
+    nb_posts = cursor.execute("""SELECT COUNT(*) as count FROM post""").fetchone()[
+        "count"
+    ]
 
-    res_follows_null = cursor.execute(
-        """SELECT COUNT(*) FROM post WHERE follows IS NULL"""
-    )
-    follows_null = res_follows_null.fetchall()
-    print(f"Number of null follows: {follows_null}")
+    follows_null = cursor.execute(
+        """SELECT COUNT(*) as nb_null_follows FROM post WHERE follows IS NULL"""
+    ).fetchone()["nb_null_follows"]
+    print(f"Number of null follows: {follows_null} out of {nb_posts} posts")
 
-    res_plays_null = cursor.execute("""SELECT COUNT(*) FROM post WHERE plays IS NULL""")
-    plays_null = res_plays_null.fetchall()
-    print(f"Number of null plays: {plays_null}")
+    plays_null = cursor.execute(
+        """SELECT COUNT(*) as nb_null_plays FROM post WHERE plays IS NULL"""
+    ).fetchone()["nb_null_plays"]
+    print(f"Number of null plays: {plays_null} out of {nb_posts} posts")
 
-    post_types_res = cursor.execute(
+    data_comment_null = cursor.execute(
+        """SELECT COUNT(*) as nb_null_comment FROM post WHERE data_comment IS NULL"""
+    ).fetchone()["nb_null_comment"]
+    print(f"Number of null data comments: {data_comment_null} out of {nb_posts} posts")
+
+    post_stats = cursor.execute(
+        """ SELECT
+            post_type,
+            impressions,
+            likes,
+            1000*likes / NULLIF(impressions, 0) as rel_likes,
+            1000*shares / NULLIF(impressions, 0) as rel_shares,
+            1000*follows / NULLIF(impressions, 0) as rel_follows,
+            1000*reach / NULLIF(impressions, 0) as rel_reach,
+            1000*saves / NULLIF(impressions, 0) as rel_saves
+            FROM post
+            ORDER BY impressions DESC, rel_likes DESC
+        """
+    ).fetchall()
+
+    return post_stats
+
+
+def analyse_posts_by_type(cursor):
+
+    post_type_stats = cursor.execute(
         """
         SELECT 
             post_type, 
-            COUNT(*) as number_of_posts,
-            AVG(impressions),
-            AVG(likes),
-            AVG(shares),
-            AVG(follows),
-            AVG(saves)
+            COUNT(*) as nb_posts,
+            AVG(impressions) as mean_impressions,
+            MIN(impressions) as min_impressions,
+            MAX(impressions) as max_impressions,
+            SUM(likes / impressions) / COUNT(*) as rel_likes,
+            AVG(likes) as mean_likes,
+            MIN(likes) as min_likes,
+            MAX(likes) as max_likes,
+            MIN(shares) as min_shares,
+            MAX(shares) as max_shares,
+            AVG(shares) as mean_shares,
+            MIN(follows) as min_follows,
+            MAX(follows) as max_follows,
+            AVG(follows) as mean_follows,
+            MIN(saves) as min_saves,
+            MAX(saves) as max_saves,
+            AVG(saves) as mean_saves
         FROM post
         GROUP BY post_type
+        ORDER BY nb_posts ASC
     """
-    )
-    post_types = post_types_res.fetchall()
-    print(f"Post types")
-    print(post_types)
+    ).fetchall()
+
+    return post_type_stats
+
+
+def get_outliers(cursor):
+    """
+    Outliers are values that fall outside of 1.5 times the range between the first and third quartile.
+    """
+    quartile_breaks = cursor.execute(
+        """ 
+        SELECT
+            impr_quartile,
+            MAX(impressions) AS quartile_break
+        FROM(
+            SELECT
+                impressions,
+                NTILE(4) OVER (ORDER BY impressions) AS impr_quartile
+            FROM post) AS quartiles
+        WHERE impr_quartile IN (1, 3)
+        GROUP BY impr_quartile
+        """
+    ).fetchall()
+    print(quartile_breaks)
+    # [{'impr_quartile': 1, 'quartile_break': 13452}, {'impr_quartile': 3, 'quartile_break': 28918}]
+
+    lower_bound = quartile_breaks[0]["quartile_break"]
+    upper_bound = quartile_breaks[1]["quartile_break"]
+    iqr = (upper_bound - lower_bound) * 1.5
+    lower = lower_bound - iqr
+    upper = upper_bound + iqr
+
+    outliers = cursor.execute(
+        """SELECT *
+                                FROM post
+                                WHERE impressions < ? OR impressions > ?""",
+        (lower, upper),
+    ).fetchall()
+    print(len(outliers))
+    return outliers
+
+
+def dict_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
 
 
 def main():
     # Recreate the database every time
     if DB_PATH.is_file():
         DB_PATH.unlink()
-    connection = sqlite3.connect(DB_NAME)
+    connection: sqlite3.Connection = sqlite3.connect(DB_PATH)
+    connection.row_factory = dict_factory
     init_db(connection)
 
     cursor = connection.cursor()
-    stat_data = analyse(cursor)
+    post_stats = analyse_posts(cursor)
+    post_type_stats = analyse_posts_by_type(cursor)
+    outliers = get_outliers(cursor)
     cursor.close()
 
-    # fig = make_subplots(
-    #     rows=2,
-    #     cols=2,
-    #     subplot_titles=[
-    #         "Customer Acquisition Cost by Channel",
-    #         "Customer Revenue by Channel",
-    #         "Customer Conversion Rate by Channel",
-    #         "Customer Lifetime Value by Channel",
-    #     ],
-    # )
-    # fig.add_trace(
-    #     go.Bar(x=[t[0] for t in stat_data], y=[t[1] for t in stat_data]), row=1, col=1
-    # )
-    # fig.add_trace(
-    #     go.Bar(x=[t[0] for t in stat_data], y=[t[2] for t in stat_data]), row=1, col=2
-    # )
-    # fig.add_trace(
-    #     go.Bar(x=[t[0] for t in stat_data], y=[t[3] for t in stat_data]), row=2, col=1
-    # )
-    # fig.add_trace(
-    #     go.Bar(x=[t[0] for t in stat_data], y=[t[4] for t in stat_data]), row=2, col=2
-    # )
+    fig = make_subplots(
+        rows=2,
+        cols=6,
+        subplot_titles=[
+            "Number of posts",
+            "Mean number of impressions",
+            "Mean number of likes",
+            "Mean number of shares",
+            "Mean number of follows",
+            "Mean number of saves",
+            "",
+            "Number of impressions",
+            "Relative number of likes per impression (*1000)",
+            "Relative number of shares per impression (*1000)",
+            "Relative number of follows per impression (*1000)",
+            "Relative number of saves per impression (*1000)",
+        ],
+    )
+    # Count of posts
+    fig.add_trace(
+        go.Bar(
+            x=[t["post_type"] for t in post_type_stats],
+            y=[t["nb_posts"] for t in post_type_stats],
+        ),
+        row=1,
+        col=1,
+    )
+    # Average impressions
+    fig.add_trace(
+        go.Bar(
+            x=[t["post_type"] for t in post_type_stats],
+            y=[t["mean_impressions"] for t in post_type_stats],
+        ),
+        row=1,
+        col=2,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=[t["post_type"] for t in post_type_stats],
+            y=[t["mean_likes"] for t in post_type_stats],
+        ),
+        row=1,
+        col=3,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=[t["post_type"] for t in post_type_stats],
+            y=[t["mean_shares"] for t in post_type_stats],
+        ),
+        row=1,
+        col=4,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=[t["post_type"] for t in post_type_stats],
+            y=[t["mean_follows"] for t in post_type_stats],
+        ),
+        row=1,
+        col=5,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=[t["post_type"] for t in post_type_stats],
+            y=[t["mean_saves"] for t in post_type_stats],
+        ),
+        row=1,
+        col=6,
+    )
 
-    # fig.show()
+    # Average impressions
+    color_dict = {"IG image": "blue", "IG reel": "red", "IG carousel": "green"}
+    colors: list[str] = [color_dict[t["post_type"]] for t in post_stats]
+    fig.add_trace(
+        go.Bar(
+            x=list(range(len(post_stats))),
+            y=[t["impressions"] for t in post_stats],
+            marker_color=colors,
+        ),
+        row=2,
+        col=2,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=list(range(len(post_stats))),
+            y=[t["rel_likes"] for t in post_stats],
+            marker_color=colors,
+        ),
+        row=2,
+        col=3,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=list(range(len(post_stats))),
+            y=[t["rel_shares"] for t in post_stats],
+            marker_color=colors,
+        ),
+        row=2,
+        col=4,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=list(range(len(post_stats))),
+            y=[t["rel_follows"] for t in post_stats],
+            marker_color=colors,
+        ),
+        row=2,
+        col=5,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=list(range(len(post_stats))),
+            y=[t["rel_saves"] for t in post_stats],
+            marker_color=colors,
+        ),
+        row=2,
+        col=6,
+    )
+
+    fig.show()
 
     connection.close()
 
